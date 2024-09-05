210fbd6d5e77c39b2ed07a26be6b2f7dbe97a0e3
tomasgo <tomaskrizanauskas@gmail.com>
2024-09-05 17:12:08 +0300

    health endpoint

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -1,8 +1,10 @@
 package routes
 
 import (
+	"net/http"
 	"travelapp/internal/config"
 	"travelapp/internal/handlers/tripplanhandler"
+	"travelapp/internal/responses"
 	"travelapp/pkg/services/attractionservice"
 
 	"github.com/labstack/echo/v4"
@@ -14,5 +16,10 @@ func InitRoutes(e *echo.Echo, config *config.Config) {
 	tripPlanHandler := tripplanhandler.New(attractionService)
 
 	e.POST("/trips/plan", tripPlanHandler.Do)
+	e.GET("/health", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, responses.DataResponse{
+			Message: http.StatusText(http.StatusOK),
+		})
+	})
 	e.Static("/uploads", "uploads")
 }
